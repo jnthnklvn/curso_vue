bbae787ed7c8eafb0dc7e13a277199884e52a560
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-11 17:03:50 -0300

    Modulo 1 de Componentes

---
+++ desafios/componente-desafio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <app-drawer />
+    <app-toolbar />
+    <v-content>
+      <Carousel />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Drawer from "@/components/template/Drawer";
+import Carousel from "@/components/widgets/Carousel";
+import Footer from "@/components/template/Footer";
+import Toolbar from "./components/template/Toolbar";
+export default {
+  components: {
+    appToolbar: Toolbar,
+    appFooter: Footer,
+    "app-drawer": Drawer,
+    Carousel
+  }
+};
+</script>
