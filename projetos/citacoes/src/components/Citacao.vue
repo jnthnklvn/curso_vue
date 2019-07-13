c613ff0580c8bf040b365d24b6f84ea8c616f506
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-13 16:31:33 -0300

    Passando conteudo no corpo com slot

---
+++ projetos/citacoes/src/components/Citacao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+        <!-- <p>{{ citacao.texto }}</p> -->
+    </div>
+</template>
+
+<script>
+export default {
+  props:  ['citacao'],
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
