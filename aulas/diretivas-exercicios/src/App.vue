127d890990e68ed94a94225f088d0ec7c989ccfe
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-15 01:41:11 -0300

    Diretiva local

--- aulas/diretivas-exercicios/src/App.vue
+++ aulas/diretivas-exercicios/src/App.vue
@@ -5,13 +5,33 @@
     <p v-text="'Texto usando v-text'"></p>
     <p v-html="'Texto usando <strong>v-html</strong>'"></p>
     <hr />
-    <p v-destaque:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>
-    <p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>
+    <p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
 <script>
 export default {
+  directives: {
+    "destaque-local": {
+      bind(el, binding, vnode) {
+        //el.style.backgroundColor = 'lightgreen'
+        let atraso = 0;
+
+        if (binding.modifiers["atrasar"]) atraso = 2000;
+
+        setTimeout(() => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = binding.value;
+          }
+          //el.style.backgroundColor = 'lightgreen'
+          else {
+            el.style.color = binding.value;
+          }
+        }, atraso);
+      }
+    }
+  },
   data() {
     return {
       cor: "lightblue"
