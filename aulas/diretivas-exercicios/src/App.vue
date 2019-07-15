31db991ed9449cb6fdd3d2be5e93abaf6bd5ad5c
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-15 01:47:15 -0300

    Personalizando diretiva

--- aulas/diretivas-exercicios/src/App.vue
+++ aulas/diretivas-exercicios/src/App.vue
@@ -6,7 +6,7 @@
     <p v-html="'Texto usando <strong>v-html</strong>'"></p>
     <hr />
     <p v-destaque-local:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>
-    <p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
+    <p v-destaque-local.atrasar.alternar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
@@ -15,18 +15,32 @@ export default {
   directives: {
     "destaque-local": {
       bind(el, binding, vnode) {
-        //el.style.backgroundColor = 'lightgreen'
+        const aplicarCor = cor => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = cor;
+          }
+          //el.style.backgroundColor = 'lightgreen'
+          else {
+            el.style.color = cor;
+          }
+        };
+
         let atraso = 0;
 
         if (binding.modifiers["atrasar"]) atraso = 2000;
 
+        let cor1 = binding.value;
+        let cor2 = "purple";
+        let corAtual = cor1;
+
         setTimeout(() => {
-          if (binding.arg === "fundo") {
-            el.style.backgroundColor = binding.value;
-          }
-          //el.style.backgroundColor = 'lightgreen'
-          else {
-            el.style.color = binding.value;
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1;
+              aplicarCor(corAtual);
+            }, 1000);
+          } else {
+            aplicarCor(binding.value);
           }
         }, atraso);
       }
