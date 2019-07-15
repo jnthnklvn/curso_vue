9925ae41e2d06f4be5abb489bdbc326fc12b70ce
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-15 01:27:23 -0300

    Diretivas

---
+++ aulas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr />
+    <p v-text="'Texto usando v-text'"></p>
+    <p v-html="'Texto usando <strong>v-html</strong>'"></p>
+	<hr>
+	<p v-destaque="'lightgreen'">Usando diretiva personalizada</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
