dfc9916d0b7564a7f8028ce1c6d5de16fca4cf88
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-07-16 19:38:11 -0300

    Mudancas parametros de rotas

--- aulas/rotas-exercicios/src/components/Usuario.vue
+++ aulas/rotas-exercicios/src/components/Usuario.vue
@@ -22,6 +22,11 @@ export default {
       this.$router.push("/");
       /* this.$router.push({ path: "/" }); */
     }
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    }
   }
 };
 </script>
--- aulas/rotas-exercicios/src/components/template/Menu.vue
+++ aulas/rotas-exercicios/src/components/template/Menu.vue
@@ -7,6 +7,9 @@
       <router-link to="/usuario/10" tag="li" active-class="active">
         <a>Usuário</a>
       </router-link>
+      <router-link to="/usuario/20" tag="li" active-class="active">
+        <a>Usuário2</a>
+      </router-link>
     </ul>
   </nav>
 </template>
