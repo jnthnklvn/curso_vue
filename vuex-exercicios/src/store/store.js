8c949808d3b58e69f0f5f5251786e5e7c788e296
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-07-18 17:05:41 -0300

    Vuex com v-model

--- vuex-exercicios/src/store/store.js
+++ vuex-exercicios/src/store/store.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         produtos: [
-        ]
+        ],
+        quantidade: 2,
+        preco: 19.99
     },
     getters: {
         valorTotal(state) {
@@ -18,10 +20,16 @@ const store = new Vuex.Store({
     mutations: {
         adicionarProduto(state, payload) {
             state.produtos.push(payload)
+        },
+        setQuantidade(state, payload) {
+            state.quantidade = payload
+        },
+        setPreco(state, payload) {
+            state.preco = payload
         }
     },
     actions: {
-        adicionarProduto(context, payload){
+        adicionarProduto(context, payload) {
             setTimeout(() => {
                 context.commit('adicionarProduto', payload)
             }, 1000)
